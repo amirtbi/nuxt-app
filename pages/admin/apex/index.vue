88dcfedb947112e0fe299c985f3063d6da78037e
amir <torabi46@gmail.com>
2022-07-25 19:13:33 +0430

    Postion to right

--- pages/admin/apex/index.vue
+++ pages/admin/apex/index.vue
@@ -31,7 +31,7 @@ export default {
           showForSingleSeries: false,
           showForNullSeries: true,
           showForZeroSeries: true,
-          position: "left",
+          position: "right",
           horizontalAlign: "center",
           floating: false,
           fontSize: "14px",
